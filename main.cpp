9e54baae9cf61be70057d7f0b5fcb93baefb6fd4
Robert Middleton <robert.middleton@rm5248.com>
2019-08-04 15:58:45 -0400

    Refresh servers on startup

--- main.cpp
+++ main.cpp
@@ -1,11 +1,14 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include <QTimer>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
+
+    QTimer::singleShot( 0, &w, &MainWindow::refresh );
 
     return a.exec();
 }
